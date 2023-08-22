0069ee955846778b63fff7c32df0e879c8becf37
samuellvn <138622449+samuellvn@users.noreply.github.com>
2023-08-22 19:52:30 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,6 +15,19 @@ reconhecimento.onresult=function(evento){
         speak1();
     }
 }
+function foto(){
+    Webcam.snap(function(data_uri){
+        if(imagem=="imagem1"){
+            document.getElementById("img1").src=data_uri;
+        }
+        if(imagem=="imagem2"){
+            document.getElementById("img2").src=data_uri;
+        }
+        if(imagem=="imagem3"){
+            document.getElementById("img3").src=data_uri;
+        }
+    });
+}
 
 function speak1(){
     var texto_fala=window.speechSynthesis;
@@ -24,6 +37,24 @@ function speak1(){
     texto_fala.speak(fala_convertida);
 
     setTimeout(function(){
-        imagem=""
+        imagem="imagem1";
+        foto();
+        fala_dado="tirando sua selfie em 5 segundos";
+        fala_convertida=new SpeechSynthesisUtterance(fala_dado);
+        texto_fala.speak(fala_convertida);
     }, 5000);
+    setTimeout(function(){
+        imagem="imagem2";
+        foto();
+        fala_dado="tirando sua selfie em 10 segundos";
+        fala_convertida=new SpeechSynthesisUtterance(fala_dado);
+        texto_fala.speak(fala_convertida);
+    }, 10000);
+    setTimeout(function(){
+        imagem="imagem3";
+        foto();
+        fala_dado="tirando sua selfie em 15 segundos";
+        fala_convertida=new SpeechSynthesisUtterance(fala_dado);
+        texto_fala.speak(fala_convertida);
+    }, 15000);
 }
